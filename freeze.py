2eaa31271317a7c1f77a46c7d2676c039491f8e2
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-12-27 22:13:56 -0500

    Updates to freeze.py

--- freeze.py
+++ freeze.py
@@ -5,7 +5,11 @@ Created on Fri Dec 27 21:55:40 2019
 @author: stany
 """
 
-from main import main 
+
+from flask_frozen import Freezer
+from app import app
+
+freezer = Freezer(app)
 
 if __name__ == '__main__':
-    main.freezer.freeze()
+    freezer.freeze()
