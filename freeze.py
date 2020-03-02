151462af205a2d7653e079aa5755a9cb0878d8a5
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2020-03-01 21:15:06 -0500

    Replaced Old File
    Replaced old files with new ones modeled after the actually functioning dsh website.

--- freeze.py
+++ freeze.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 27 21:55:40 2019
-
-@author: stany
-"""
-
-# This builds the static content to the project root 
-
+# This builds the static content to the project root.
 from project import main
-
-freezer = Freezer(app)
 
 if __name__ == '__main__':
     main.freezer.freeze()
